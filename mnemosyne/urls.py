b3667ee2ed97362f444edc66dc8e9c179e9d6703
Ahad <mohdahad60080@gmail.com>
2026-01-22 17:26:49 +0530

    made home page and building additional pages

--- mnemosyne/urls.py
+++ mnemosyne/urls.py
@@ -6,5 +6,7 @@ from . import views #apps view
 app_name = 'mnemosyne' #name spacing
 urlpatterns = [
     #Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    #path to all topics
+    path('topics/', views.topics, name='topics'),
 ]
--- mnemosyne/views.py
+++ mnemosyne/views.py
@@ -6,6 +6,13 @@ Sends it back to the browser
 """
 
 from django.shortcuts import render
+from .models import Topic
 
 def index(request):
+    #Inside the templates/ folder, look inside the mnemosyne namespace
     return render(request, 'mnemosyne/index.html')
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'mnemosyne/topics.html', context)
