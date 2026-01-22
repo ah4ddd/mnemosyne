7ef64e94799a0f482f3181b75938a4dae2664300
Ahad <mohdahad60080@gmail.com>
2026-01-22 18:56:04 +0530

    added individual topic pages

--- mnemosyne/urls.py
+++ mnemosyne/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     #path to all topics
     path('topics/', views.topics, name='topics'),
+    #detail page for single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- mnemosyne/views.py
+++ mnemosyne/views.py
@@ -16,3 +16,9 @@ def topics(request):
     topics = Topic.objects.order_by('date_added')
     context = {'topics': topics}
     return render(request, 'mnemosyne/topics.html', context)
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added') # type: ignore
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'mnemosyne/topic.html', context)
