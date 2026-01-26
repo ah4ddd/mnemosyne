be3f40982468e1412e847aaf59d8d362843aaddf
Ahad <mohdahad60080@gmail.com>
2026-01-26 19:46:20 +0530

    Restricting Topics Access to Appropriate Users

--- mnemosyne/views.py
+++ mnemosyne/views.py
@@ -17,7 +17,7 @@ def index(request):
 # restrics data to logged in users
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'mnemosyne/topics.html', context)
 
